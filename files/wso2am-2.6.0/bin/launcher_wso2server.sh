18f0123dff3d67b5b601f3456fcf8c9bb426674d
Luis Dipotet <ldipotet.job@gmail.com>
2019-02-04 12:56:03 +0000

    adding docker conf

---
+++ files/wso2am-2.6.0/bin/launcher_wso2server.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# ----------------------------------------------------------------------------
+#  Copyright 2005-2012 WSO2, Inc. http://www.wso2.org
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+# ----------------------------------------------------------------------------
+
+#Find product home location
+PRG="$0"
+while [ -h "$PRG" ]; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '.*/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+
+PRGDIR=`dirname "$PRG"`
+PRODUCT_HOME=`cd "$PRGDIR/.." ; pwd`
+
+#If JAVA_HOME is not defined, introduce new JAVA_HOME from product home
+[ -z "$JAVA_HOME" ] && JAVA_HOME="$PRODUCT_HOME/jdk/jdk1.8.0_192"
+export JAVA_HOME=$JAVA_HOME
+
+#Start Product
+bash $PRODUCT_HOME/bin/wso2server.sh $@
+
